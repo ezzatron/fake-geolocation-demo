55e40c30a3bcc151b6bc73ea7df64299a35562c3
Erin Millard <contact@erin.id.au>
2024-05-31 15:46:40 +1000

    WIP

--- test/vitest/journey.spec.ts
+++ test/vitest/journey.spec.ts
@@ -1,61 +1,157 @@
 import { beforeEach, describe, expect, it } from "vitest";
 import { createJourney, type Journey } from "../../src/journey";
 
-describe("createJourney()", () => {
-  let journey: Journey;
+const ZERO_COORDS: GeolocationCoordinates = {
+  latitude: 0,
+  longitude: 0,
+  altitude: null,
+  accuracy: 0,
+  altitudeAccuracy: null,
+  heading: null,
+  speed: null,
+};
 
-  beforeEach(() => {
-    journey = createJourney(
-      {
-        coords: {
-          latitude: 0,
-          longitude: 0,
-          altitude: 0,
-          accuracy: 10,
-          altitudeAccuracy: 10,
-          heading: null,
-          speed: null,
-        },
-        timestamp: 0,
-      },
-      {
-        coords: {
-          latitude: 1,
-          longitude: 1,
-          altitude: 1,
-          accuracy: 10,
-          altitudeAccuracy: 10,
-          heading: null,
-          speed: null,
-        },
-        timestamp: 100,
-      },
-    );
+function createCoordinates(
+  coords: Partial<GeolocationCoordinates>,
+): GeolocationCoordinates {
+  return { ...ZERO_COORDS, ...coords };
+}
+
+describe("createJourney()", () => {
+  it("throws if no positions are provided", () => {
+    expect(() => createJourney()).toThrowError("No positions provided");
+    expect(() => createJourney()).toThrowError(TypeError);
   });
 
-  describe("coordsAtRatio()", () => {
-    it("should return the position at the start of the journey", () => {
-      const coords = journey.coordsAtTime(0);
+  describe("coordsAtTime()", () => {
+    let journey: Journey;
 
-      expect(coords.latitude).toBeCloseTo(0, 10);
-      expect(coords.longitude).toBeCloseTo(0, 10);
-      expect(coords.altitude).toBeCloseTo(0, 10);
+    beforeEach(() => {
+      journey = createJourney(
+        {
+          coords: createCoordinates({
+            latitude: 90,
+            longitude: -150,
+            altitude: 100,
+            accuracy: 100,
+            altitudeAccuracy: 100,
+          }),
+          timestamp: 100,
+        },
+        {
+          coords: createCoordinates({
+            latitude: 90,
+            longitude: 150,
+            altitude: 200,
+            accuracy: 200,
+            altitudeAccuracy: 200,
+          }),
+          timestamp: 125,
+        },
+        {
+          coords: createCoordinates({
+            latitude: 90,
+            longitude: 50,
+            altitude: 300,
+            accuracy: 300,
+            altitudeAccuracy: 300,
+          }),
+          timestamp: 200,
+        },
+      );
     });
 
-    it("should return the position at the end of the journey", () => {
-      const coords = journey.coordsAtTime(100);
-
-      expect(coords.latitude).toBeCloseTo(1, 10);
-      expect(coords.longitude).toBeCloseTo(1, 10);
-      expect(coords.altitude).toBeCloseTo(1, 10);
-    });
-
-    it("should return the position at the middle of the journey", () => {
+    it("returns the first position before the start time", () => {
       const coords = journey.coordsAtTime(50);
 
-      expect(coords.latitude).toBeCloseTo(0.5509374423857688, 10);
-      expect(coords.longitude).toBeCloseTo(0.33838006814982347, 10);
-      expect(coords.altitude).toBeCloseTo(0.5, 10);
+      expect(coords).toMatchObject({
+        latitude: expect.closeTo(90, 10) as number,
+        longitude: expect.closeTo(-150, 10) as number,
+        altitude: expect.closeTo(100, 10) as number,
+        accuracy: expect.closeTo(100, 10) as number,
+        altitudeAccuracy: expect.closeTo(100, 10) as number,
+      });
     });
+
+    it("returns the first position at the start time", () => {
+      const coords = journey.coordsAtTime(100);
+
+      expect(coords).toMatchObject({
+        latitude: expect.closeTo(90, 10) as number,
+        longitude: expect.closeTo(-150, 10) as number,
+        altitude: expect.closeTo(100, 10) as number,
+        accuracy: expect.closeTo(100, 10) as number,
+        altitudeAccuracy: expect.closeTo(100, 10) as number,
+      });
+    });
+
+    it("returns a mid position when at that position's time", () => {
+      const coords = journey.coordsAtTime(125);
+
+      expect(coords).toMatchObject({
+        latitude: expect.closeTo(90, 10) as number,
+        longitude: expect.closeTo(150, 10) as number,
+        altitude: expect.closeTo(200, 10) as number,
+        accuracy: expect.closeTo(200, 10) as number,
+        altitudeAccuracy: expect.closeTo(200, 10) as number,
+      });
+    });
+
+    it("returns the last position at the end time", () => {
+      const coords = journey.coordsAtTime(200);
+
+      expect(coords).toMatchObject({
+        latitude: expect.closeTo(90, 10) as number,
+        longitude: expect.closeTo(50, 10) as number,
+        altitude: expect.closeTo(300, 10) as number,
+        accuracy: expect.closeTo(300, 10) as number,
+        altitudeAccuracy: expect.closeTo(300, 10) as number,
+      });
+    });
+
+    it("returns the last position after the end time", () => {
+      const coords = journey.coordsAtTime(250);
+
+      expect(coords).toMatchObject({
+        latitude: expect.closeTo(90, 10) as number,
+        longitude: expect.closeTo(50, 10) as number,
+        altitude: expect.closeTo(300, 10) as number,
+        accuracy: expect.closeTo(300, 10) as number,
+        altitudeAccuracy: expect.closeTo(300, 10) as number,
+      });
+    });
+
+    it.each`
+      t        | latitude | longitude | altitude | accuracy | altitudeAccuracy
+      ${112.5} | ${90}    | ${180}    | ${150}   | ${150}   | ${150}
+      ${162.5} | ${90}    | ${100}    | ${250}   | ${250}   | ${250}
+    `(
+      "returns the interpolated position at time = $t",
+      ({
+        t,
+        latitude,
+        longitude,
+        altitude,
+        accuracy,
+        altitudeAccuracy,
+      }: {
+        t: number;
+        latitude: number;
+        longitude: number;
+        altitude: number;
+        accuracy: number;
+        altitudeAccuracy: number;
+      }) => {
+        const coords = journey.coordsAtTime(t);
+
+        expect(coords).toMatchObject({
+          latitude: expect.closeTo(latitude, 10) as number,
+          longitude: expect.closeTo(longitude, 10) as number,
+          altitude: expect.closeTo(altitude, 10) as number,
+          accuracy: expect.closeTo(accuracy, 10) as number,
+          altitudeAccuracy: expect.closeTo(altitudeAccuracy, 10) as number,
+        });
+      },
+    );
   });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,11 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      include: ["src/**/*.ts"],
+    },
     globals: true,
+    include: ["test/vitest/**/*.spec.ts"],
     environment: "jsdom",
     setupFiles: ["./test/vitest/initialize.ts"],
   },
