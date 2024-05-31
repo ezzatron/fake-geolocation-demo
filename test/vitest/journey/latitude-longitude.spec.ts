48abe9d25166508e620734cd3a73180986dc72b6
Erin Millard <contact@erin.id.au>
2024-05-31 17:58:33 +1000

    WIP

--- test/vitest/journey/latitude-longitude.spec.ts
+++ test/vitest/journey/latitude-longitude.spec.ts
@@ -1,71 +1,38 @@
-import { describe, expect, it } from "vitest";
+import { expect, it } from "vitest";
 import { createJourney } from "../../../src/journey";
 import { createCoordinates } from "./util";
 
-describe("when latitude is constant", () => {
-  const journey = createJourney(
-    {
-      coords: createCoordinates({ latitude: 90, longitude: -170 }),
-      timestamp: 0,
-    },
-    {
-      coords: createCoordinates({ latitude: 90, longitude: 170 }),
-      timestamp: 50,
-    },
-    {
-      coords: createCoordinates({ latitude: 90, longitude: 70 }),
-      timestamp: 150,
-    },
-  );
+const journey = createJourney(
+  {
+    coords: createCoordinates({ latitude: 70, longitude: -170 }),
+    timestamp: 0,
+  },
+  {
+    coords: createCoordinates({ latitude: 80, longitude: 170 }),
+    timestamp: 50,
+  },
+  {
+    coords: createCoordinates({ latitude: 90, longitude: 70 }),
+    timestamp: 150,
+  },
+);
 
-  it.each([
-    [-25, -170],
-    [0, -170],
-    [25, 180],
-    [50, 170],
-    [75, 150.78973302883216],
-    [100, 120],
-    [125, 89.21026697116784],
-    [150, 70],
-    [175, 70],
-  ])("linearly interpolates the coordinates for time = %s", (t, longitude) => {
-    expect(journey.coordsAtTime(t)).toMatchObject({
-      latitude: 90,
-      longitude: expect.closeTo(longitude, 10) as number,
-    });
-  });
-});
-
-describe("when longitude is constant", () => {
-  const journey = createJourney(
-    {
-      coords: createCoordinates({ latitude: 70, longitude: 180 }),
-      timestamp: 0,
-    },
-    {
-      coords: createCoordinates({ latitude: 80, longitude: 180 }),
-      timestamp: 50,
-    },
-    {
-      coords: createCoordinates({ latitude: 90, longitude: 180 }),
-      timestamp: 150,
-    },
-  );
-
-  it.each([
-    [-25, 70],
-    [0, 70],
-    [25, 75],
-    [50, 80],
-    [75, 82.4952312784634],
-    [100, 85],
-    [125, 87.5047687215366],
-    [150, 90],
-    [175, 90],
-  ])("linearly interpolates the coordinates for time = %s", (t, latitude) => {
+it.each([
+  [-25, 70, -170],
+  [0, 70, -170],
+  [25, 75.19442943503938, -176.70495327058325],
+  [50, 80, 170],
+  [75, 82.4952312784634, 170],
+  [100, 85, 170],
+  [125, 87.5047687215366, 170],
+  [150, 90, 70],
+  [175, 90, 70],
+])(
+  "linearly interpolates the coordinates for time = %s",
+  (t, latitude, longitude) => {
     expect(journey.coordsAtTime(t)).toMatchObject({
       latitude: expect.closeTo(latitude, 10) as number,
-      longitude: 180,
+      longitude: expect.closeTo(longitude, 10) as number,
     });
-  });
-});
+  },
+);
