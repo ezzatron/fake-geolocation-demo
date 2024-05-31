097b48cfd37504352189f28ccd7486ba246b67e6
Erin Millard <contact@erin.id.au>
2024-05-31 13:30:23 +1000

    WIP

--- src/journey.ts
+++ src/journey.ts
@@ -6,22 +6,29 @@ import {
 } from "nvector-geodesy";
 
 export type Journey = {
-  coordsAtRatio: (ratio: number) => GeolocationCoordinates;
+  coordsAtTime: (ratio: number) => GeolocationCoordinates;
 };
 
 export function createJourney(
-  aCoords: GeolocationCoordinates,
-  bCoords: GeolocationCoordinates,
+  a: GeolocationPosition,
+  b: GeolocationPosition,
 ): Journey {
-  const a = fromGeodeticCoordinates(aCoords.longitude, aCoords.latitude);
-  const aAlt = aCoords.altitude ?? 0;
-  const b = fromGeodeticCoordinates(bCoords.longitude, bCoords.latitude);
-  const bAlt = bCoords.altitude ?? 0;
-
   return {
-    coordsAtRatio: (ratio: number) => {
+    coordsAtTime: (t: number) => {
+      const aNV = fromGeodeticCoordinates(
+        a.coords.longitude,
+        a.coords.latitude,
+      );
+      const aAlt = a.coords.altitude ?? 0;
+      const bNV = fromGeodeticCoordinates(
+        b.coords.longitude,
+        b.coords.latitude,
+      );
+      const bAlt = b.coords.altitude ?? 0;
+
+      const ratio = (t - a.timestamp) / (b.timestamp - a.timestamp);
       const [latitude, longitude] = toGeodeticCoordinates(
-        normalize(apply((a, b) => a + (b - a) * ratio, a, b)),
+        normalize(apply((a, b) => a + (b - a) * ratio, aNV, bNV)),
       );
       const altitude = aAlt + (bAlt - aAlt) * ratio;
 
