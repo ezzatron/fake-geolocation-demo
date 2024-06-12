1ee63dc3075d19e4cc489356104e11a1fcd9cb29
Erin Millard <contact@erin.id.au>
2024-06-13 00:38:52 +1000

    WIP

---
+++ src/components/CompassPointer.tsx
@@ -0,0 +1,11 @@
+export default function CompassPointer() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 60 60"
+      id="compass-pointer"
+    >
+      <polygon points="30,3 33,10 27,10" fill="var(--blue-6)" />
+    </svg>
+  );
+}
