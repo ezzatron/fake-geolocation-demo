16d930368dc0b68a4a0870ffdea617e2e95b8c9e
Erin Millard <contact@erin.id.au>
2024-06-13 13:16:08 +1000

    WIP

--- src/components/CompassPointer.tsx
+++ src/components/CompassPointer.tsx
@@ -6,6 +6,15 @@ export default function CompassPointer() {
       id="compass-pointer"
     >
       <polygon points="30,3 33,10 27,10" fill="var(--blue-6)" />
+      <line
+        x1="30"
+        y1="10"
+        x2="30"
+        y2="55"
+        stroke="var(--blue-8)"
+        strokeWidth="1"
+        strokeDasharray="2 3"
+      />
     </svg>
   );
 }
