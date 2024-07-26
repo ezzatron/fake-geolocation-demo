251970c692cf1b207b87879472f0f91acf0ed50a
Erin Millard <contact@erin.id.au>
2024-07-26 10:48:38 +1000

    WIP

--- test/extract-gopro-journey.mjs
+++ test/extract-gopro-journey.mjs
@@ -9,24 +9,23 @@ if (!filePath) {
   throw new Error(`Usage: ${basename(scriptName)} <path to video file>`);
 }
 
-const GPMF = await GPMFExtract(bufferAppender(filePath, 1 << 16));
+const GPMF = await GPMFExtract((file) => {
+  const stream = createReadStream(filePath, { highWaterMark: 1 << 16 });
+  let bytesRead = 0;
+
+  stream.on("end", () => {
+    file.flush();
+  });
+
+  stream.on("data", (chunk) => {
+    const arrayBuffer = new Uint8Array(chunk).buffer;
+    arrayBuffer.fileStart = bytesRead;
+    file.appendBuffer(arrayBuffer);
+    bytesRead += chunk.length;
+  });
+
+  stream.resume();
+});
 
 console.log("Length of data received:", GPMF.rawData.length);
 console.log("Framerate of data received:", 1 / GPMF.timing.frameDuration);
-
-function bufferAppender(path, chunkSize) {
-  return function (mp4boxFile) {
-    var stream = createReadStream(path, { highWaterMark: chunkSize });
-    var bytesRead = 0;
-    stream.on("end", () => {
-      mp4boxFile.flush();
-    });
-    stream.on("data", (chunk) => {
-      var arrayBuffer = new Uint8Array(chunk).buffer;
-      arrayBuffer.fileStart = bytesRead;
-      mp4boxFile.appendBuffer(arrayBuffer);
-      bytesRead += chunk.length;
-    });
-    stream.resume();
-  };
-}
